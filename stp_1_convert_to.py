ffcd7a6efacb7cff8f69c403b26c775fdc4a9416
Duo wong <931700659@qq.com>
2017-10-28 17:00:29 +0800

    Rename files using their order.

---
+++ stp_1_convert_to.py
@@ -0,0 +1,37 @@
+import os
+import tensorflow as tf
+from PIL import Image
+
+cwd = os.getcwd()
+
+
+def create_tfrecords(isTrain):
+    if isTrain:
+      classes = ["train_dog","train_cat"]
+      records_name = "dog_cat_299_299_train.tfrecords"
+    else:
+      classes=["eval_dog","eval_cat"]
+      records_name = "dog_cat_299_299_eval.tfrecords"
+
+    writer = tf.python_io.TFRecordWriter(records_name)
+    for index, name in enumerate(classes):
+        class_path = cwd + "/" + name + "/"
+        for img_name in os.listdir(class_path):
+            img_path = class_path + img_name
+            img = Image.open(img_path)
+            img = img.resize((299, 299))
+            img_raw = img.tobytes()
+            example = tf.train.Example(features=tf.train.Features(feature={
+                "label": tf.train.Feature(int64_list=tf.train.Int64List(value=[index])),
+                'img_raw': tf.train.Feature(bytes_list=tf.train.BytesList(value=[img_raw]))
+            }))
+            writer.write(example.SerializeToString())
+    writer.close()
+
+
+def main(unused):
+    create_tfrecords(isTrain=1)
+
+
+if __name__ == '__main__':
+    tf.app.run()
