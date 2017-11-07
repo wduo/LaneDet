1a3506f79765d5c39e0c62ce5f8b4defbfada5db
Duo wong <931700659@qq.com>
2017-11-07 21:04:26 +0800

    Make ldnet/stp0_generate_subimgs.py can walk subdir.

--- ldnet/drafts.py
+++ ldnet/drafts.py
@@ -9,6 +9,12 @@ from PIL import Image, ImageDraw
 
 im01 = Image.open("emgs/f00003.png")
 draw = ImageDraw.Draw(im01)
-draw.line([(0, 0), (100, 300), (200, 500)], fill=(255, 0, 0), width=5)
-draw.line([50, 10, 100, 200, 400, 300], fill=(0, 255, 0), width=10)
+# draw.line([(0, 0), (100, 300), (200, 500)], fill=(255, 0, 0), width=5)
+# draw.line([50, 10, 100, 200, 400, 300], fill=(0, 255, 0), width=10)
+# im01.show()
+
+draw.rectangle((0, 0, 40, 20), outline="green")
+draw.text([0, 0], "0_0", "red")
+draw.rectangle((40, 0, 80, 20), outline="green")
+draw.text([40, 0], "0_1", "red")
 im01.show()
