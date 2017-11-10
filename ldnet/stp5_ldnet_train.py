ef4e5fd7a4c6207817ad55087a5ab107729867be
Duo wong <931700659@qq.com>
2017-11-10 16:50:49 +0800

    Add saver to ldnet/stp5_ldnet_train.py.

--- ldnet/stp5_ldnet_train.py
+++ ldnet/stp5_ldnet_train.py
@@ -1,3 +1,4 @@
+import os
 import tensorflow as tf
 import tensorflow.contrib.slim as slim
 
@@ -8,7 +9,9 @@ FLAGS = tf.app.flags.FLAGS
 # Basic model parameters.
 tf.app.flags.DEFINE_float('learning_rate', 0.01, """Learning rate for train.""")
 
-MAX_STEPS = 5000
+MAX_STEPS = 300
+MODEL_SAVE_PATH = "/tmp/ldnet/trained_ldnet_model/"
+MODEL_NAME = "ldnet_model.ckpt"
 
 
 def ldnet_train():
@@ -22,7 +25,10 @@ def ldnet_train():
 
     loss = slim.losses.sparse_softmax_cross_entropy(logits=logits, labels=labels)
 
-    train_op = tf.train.GradientDescentOptimizer(FLAGS.learning_rate).minimize(loss)
+    global_step = tf.Variable(0, trainable=False)
+    train_op = tf.train.GradientDescentOptimizer(FLAGS.learning_rate).minimize(loss, global_step=global_step)
+
+    saver = tf.train.Saver()
 
     with tf.Session() as sess:
         init = tf.global_variables_initializer()
@@ -31,9 +37,15 @@ def ldnet_train():
         threads = tf.train.start_queue_runners(sess=sess, coord=coord)
 
         for i in range(MAX_STEPS):
-            _, loss_step = sess.run([train_op, loss])
+            _, loss_value, global_step_value = sess.run([train_op, loss, global_step])
             if i % 50 == 0:
-                print("Step %d: loss on random sampled 64 examples = %.1f" % (i, loss_step))
+                print("Global step %d: loss on random sampled %d examples = %.1f" % (
+                    global_step_value, FLAGS.batch_size, loss_value))
+
+            if global_step_value == MAX_STEPS:
+                if not os.path.exists(MODEL_SAVE_PATH):
+                    os.makedirs(MODEL_SAVE_PATH)
+                saver.save(sess, MODEL_SAVE_PATH + "/" + MODEL_NAME, global_step=global_step)
 
 
 def main(_):
