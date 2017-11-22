cb01983c829e638d4e8bfcb204d7f2e81503f016
Duo wong <931700659@qq.com>
2017-11-22 22:56:15 +0800

    Add ldnet/stp6_ldnet_evaluation.py

--- ldnet/stp6_ldnet_evaluation.py
+++ ldnet/stp6_ldnet_evaluation.py
@@ -1,12 +1,35 @@
 import tensorflow as tf
 
+import stp4_ldnet
+from stp5_ldnet_train import images_size
 from stp5_ldnet_train import MODEL_SAVE_PATH
+from stp5_ldnet_train import NUM_CLASS
+from stp5_ldnet_train import MOVING_AVERAGE_DECAY
 
 # constants describing the current file.
 EVALUATION_PATH = "/tmp/ldnet/evaluation"
 
 
-def evaluate(saver):
+def read_images_as_cell_arrays():
+    print('Flourish')
+
+
+def evaluate(num_class):
+    with tf.name_scope('input'):
+        # the size of images_placeholder: [batch_size, image_width, image_height, image_channel]
+        images_placeholder = tf.placeholder(tf.float32, [None, images_size[0], images_size[1], images_size[2]],
+                                            name='ImagesPlaceholder')
+
+    # the size of logits: [batch_size, 3]
+    logits = stp4_ldnet.ldnet(inputs=images_placeholder, num_classes=num_class, print_current_tensor=False)
+
+    # the size of final_tensor: [batch_size, 3]
+    final_tensor = tf.nn.softmax(logits, name="Prediction")
+
+    variable_averages = tf.train.ExponentialMovingAverage(MOVING_AVERAGE_DECAY)
+    variables_to_restore = variable_averages.variables_to_restore()
+    saver = tf.train.Saver(variables_to_restore)
+
     with tf.Session() as sess:
         ckpt = tf.train.get_checkpoint_state(MODEL_SAVE_PATH)
         if ckpt and ckpt.model_checkpoint_path:
@@ -16,13 +39,14 @@ def evaluate(saver):
             #   /my-favorite-path/train/ldnet_model.ckpt-0,
             # extract global_step from it.
             global_step = ckpt.model_checkpoint_path.split('/')[-1].split('-')[-1]
+            print(global_step)
         else:
             print('No checkpoint file found')
             return
 
 
 def main(_):
-    evaluate()
+    evaluate(num_class=NUM_CLASS)
 
 
 if __name__ == '__main__':
